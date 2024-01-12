45a2cfed68a3a0771036c723404d027962ecb0c9
Gela Samsonidze <samsonidze7@yahoo.com>
2024-01-12 12:51:20 +0400

    data

--- components/ShareComp.tsx
+++ components/ShareComp.tsx
@@ -1,21 +1,39 @@
 "use client";
 
-
 import { fullBlog } from "@/app/blog/page";
-import { FacebookShareButton, FacebookIcon } from "next-share";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+  TwitterShareButton,
+  TwitterIcon,
+} from "next-share";
 
-const ShareComp = ({data}: {data: fullBlog}) => {
-
+const ShareComp = ({ data }: { data: fullBlog }) => {
   return (
-    <div style={{marginTop: "50px"}}>
+    <div style={{ marginTop: "50px" }}>
       <FacebookShareButton
         url={`https://next-marlin.vercel.app/blog/${data.currentSlug}`}
         quote={data.title}
         windowHeight={500}
         windowWidth={500}
       >
-        <FacebookIcon size={32} round />
+        <FacebookIcon size={32} />
       </FacebookShareButton>
+
+      <LinkedinShareButton
+        url={`https://next-marlin.vercel.app/blog/${data.currentSlug}`}
+      >
+        <LinkedinIcon size={32} />
+      </LinkedinShareButton>
+
+      <TwitterShareButton
+        url={`https://next-marlin.vercel.app/blog/${data.currentSlug}`}
+        title={data.title}
+      >
+        <TwitterIcon size={32} />
+      </TwitterShareButton>
     </div>
   );
 };
