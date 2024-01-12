a48dabca0f29ecbb152a79b82442e3041ab28212
Gela Samsonidze <samsonidze7@yahoo.com>
2024-01-12 11:24:58 +0400

    .

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,20 @@ import MiddleSections from "@/components/MiddleSections";
 import ManageOrders from "@/components/ManageOrders";
 import Trends from "@/components/Trends";
 import ShareComp from "@/components/ShareComp";
+import { Metadata } from "next";
 
 
 export const revalidate  = 0
+
+
+export const metadata: Metadata = {
+  title: 'Simplifying and cheapenin communications between retailers and their vendors',
+  description: 'Marlin is a digital platform that connects FMCG retailers to their vendors and helps them manage orders, order statuses, product catalogs, available stocks, special offers and much more, in a single space. ',
+  openGraph: {
+    title: 'Simplifying and cheapenin communications between retailers and their vendors',
+    description: 'Marlin is a digital platform that connects FMCG retailers to their vendors and helps them manage orders, order statuses, product catalogs, available stocks, special offers and much more, in a single space. ',
+  },
+}
 
 
 export default function Home() {
