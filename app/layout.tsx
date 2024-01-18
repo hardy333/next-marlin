90817b59f895fc78779b4abefc1ff64432d77758
Gela Samsonidze <samsonidze7@yahoo.com>
2024-01-18 18:11:58 +0400

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,25 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./blog/[slug]/style.css";
-import "./blog/blog.css"
-import "./blog/blog-posts-section.css"
+import "./blog/blog.css";
+import "./blog/blog-posts-section.css";
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/react";
 
-
 export const metadata: Metadata = {
-  title: 'Simplifying and cheapenin communications between retailers and their vendors',
-  description: 'Marlin is a digital platform that connects FMCG retailers to their vendors and helps them manage orders, order statuses, product catalogs, available stocks, special offers and much more, in a single space. ',
+  title:
+    "Simplifying and cheapenin communications between retailers and their vendors",
+  description:
+    "Marlin is a digital platform that connects FMCG retailers to their vendors and helps them manage orders, order statuses, product catalogs, available stocks, special offers and much more, in a single space. ",
   openGraph: {
-    title: 'Simplifying and cheapenin communications between retailers and their vendors',
-    description: 'Marlin is a digital platform that connects FMCG retailers to their vendors and helps them manage orders, order statuses, product catalogs, available stocks, special offers and much more, in a single space. ',
+    title:
+      "Simplifying and cheapenin communications between retailers and their vendors",
+    description:
+      "Marlin is a digital platform that connects FMCG retailers to their vendors and helps them manage orders, order statuses, product catalogs, available stocks, special offers and much more, in a single space. ",
   },
-}
-
-
+};
 
 export default function RootLayout({
   children,
@@ -33,6 +34,13 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script>
+        <div
+          className="d365-mkt-config"
+          style={{ display: "none" }}
+          data-website-id="OVIn_1QnbnnbAHtF0wNZPPhPTxN8jokE5JFuyzkI3mk"
+          data-hostname="3dbdc4e02c174de9882917a6978be3f0.svc.dynamics.com"
+        ></div>
       </body>
     </html>
   );
