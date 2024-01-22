139535d4142e56a9de7bf5c5bb6dbae763d6799a
Gela Samsonidze <samsonidze7@yahoo.com>
2024-01-22 11:21:20 +0400

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,13 +3,16 @@ import fb from "@/assets/fb.png";
 import insta from "@/assets/insta.png";
 import youtube from "@/assets/youtube.png";
 import Image from "next/image";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareInstagram, FaSquareYoutube } from "react-icons/fa6";
+import { IoLogoYoutube } from "react-icons/io5";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div
         className="container-small footer__container"
-        style={{ paddingBottom: "70px" }}
+        style={{ paddingBottom: "0px" }}
       >
         <a href="#" className="footer__logo" data-aos="fade-in">
           <MarlinLogoSvg />
@@ -17,17 +20,20 @@ const Footer = () => {
         <ul className="footer__links">
           <li data-aos="fade-right" data-aos-delay={200}>
             <a href="#">
-              <Image src={fb} alt="" />
+              {/* <Image src={fb} alt="" /> */}
+              <FaFacebookSquare />
             </a>
           </li>
           <li data-aos="fade-in" data-aos-delay={200}>
-            <a href="#">
-              <Image src={insta} alt="" />
+            <a href="">
+              {/* <Image src={insta} alt="" /> */}
+              <FaSquareInstagram />
             </a>
           </li>
           <li data-aos="fade-left" data-aos-delay={200}>
             <a href="#">
-              <Image src={youtube} alt="" />
+              {/* <Image src={youtube} alt="" /> */}
+              <FaSquareYoutube />
             </a>
           </li>
         </ul>
