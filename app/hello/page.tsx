a213f715852cba167092e103acfc0b4c73fdf1a0
gela samsonidze <samsonidze14@gmail.com>
2024-02-09 12:40:23 +0400

    hello page

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Hello = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vel esse,
+        quidem similique excepturi ad assumenda, nesciunt autem molestias
+        tempora deserunt. Laudantium nam maxime nisi sit soluta vero libero
+        ipsum.
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam,
+        accusantium vel tenetur beatae excepturi quasi distinctio ipsam dolorum
+        odit deserunt temporibus laudantium optio omnis obcaecati atque
+        voluptatibus rerum hic asperiores tempora neque possimus a magnam nam!
+        Impedit aliquam beatae quam!
+      </p>
+    </div>
+  );
 };
 
 export default Hello;
